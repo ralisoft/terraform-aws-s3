403e82935394064d0a006f6615b637cc595a5f44
Lee Hull <lee5i3@leehull.com>
2020-02-28 00:02:24 -0500

    Changes unrelated to a task

--- policy.tf
+++ policy.tf
@@ -9,7 +9,31 @@ resource "aws_s3_bucket_policy" "bucket" {
   policy = var.s3_bucket_policy
 }
 
-resource "aws_s3_bucket_policy" "https" {
+resource "aws_s3_bucket_policy" "public" {
+  count = var.enabled && var.s3_public_access ? 1 : 0
+
+  bucket = aws_s3_bucket.bucket[0].id
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+      {
+        "Sid" : "PublicRead",
+        "Effect" : "Allow",
+        "Principal" : "*",
+        "Action" : "s3:GetObject",
+        "Resource" : [
+          "arn:aws:s3:::${aws_s3_bucket.bucket[0].id}",
+          "arn:aws:s3:::${aws_s3_bucket.bucket[0].id}/*"
+        ]
+      }
+    ]
+  }
+  EOF
+}
+
+resource "aws_s3_bucket_policy" "ssl" {
   count = var.enabled ? 1 : 0
 
   bucket = aws_s3_bucket.bucket[0].id
