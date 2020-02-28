ca9d13f152e00feb2d1e734de29d6001d9351a3a
Lee Hull <lee5i3@leehull.com>
2020-02-27 23:54:06 -0500

    Changes unrelated to a task

--- policy.tf
+++ policy.tf
@@ -8,3 +8,29 @@ resource "aws_s3_bucket_policy" "bucket" {
 
   policy = var.s3_bucket_policy
 }
+
+resource "aws_s3_bucket_policy" "https" {
+  count = var.enabled ? 1 : 0
+
+  bucket = aws_s3_bucket.bucket[0].id
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+      {
+        "Sid" : "OnlyAllowSSL",
+        "Effect" : "Deny",
+        "Principal" : "*",
+        "Action" : "s3:*",
+        "Resource" : "arn:aws:s3:::${aws_s3_bucket.bucket[0].id}/*",
+        "Condition": {
+            "Bool": {
+                "aws:SecureTransport": "false"
+            }
+        }        
+      }
+    ]
+  }
+EOF
+}
