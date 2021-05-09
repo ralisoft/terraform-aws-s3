4ea74730008e0bf984ebe3a552962a3177c3fa5a
Lee Hull <lee5i3@leehull.com>
2021-05-09 16:42:05 -0400

    Changes unrelated to a task

--- locals.tf
+++ locals.tf
@@ -7,14 +7,12 @@ locals {
 
 locals {
   template_dir = "${path.module}/templates"
-  
   template_vars = {
     encrypt               = var.s3_require_encryption_enabled
     bucket_arn            = aws_s3_bucket.bucket.arn
     extra_statements      = var.s3_bucket_policy
     cloudfront_identities = var.s3_cloudfront_identities
   }
-
   policy      = templatefile("${local.template_dir}/policy.tpl", local.template_vars)
   bucket_name = var.s3_bucket_name != "" ? var.s3_bucket_name : "${var.aws_account}-${var.s3_bucket_prefix}-${var.aws_region}"
 }
